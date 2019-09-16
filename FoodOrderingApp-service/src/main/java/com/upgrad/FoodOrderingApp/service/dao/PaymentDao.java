570a565ca00f1e24a7f544ba69146e8f53052b8d
UpgradAssignment <upgrad.alpha@gmail.com>
2019-09-16 20:35:59 +0530

    Payment and Ordering funtionality

---
+++ FoodOrderingApp-service/src/main/java/com/upgrad/FoodOrderingApp/service/dao/PaymentDao.java
@@ -0,0 +1,35 @@
+package com.upgrad.FoodOrderingApp.service.dao;
+
+import com.upgrad.FoodOrderingApp.service.entity.PaymentEntity;
+import org.springframework.stereotype.Repository;
+
+import javax.persistence.EntityManager;
+import javax.persistence.NoResultException;
+import javax.persistence.PersistenceContext;
+import java.util.List;
+
+
+@Repository
+public class PaymentDao {
+
+	@PersistenceContext
+	private EntityManager entityManager;
+
+
+	public List<PaymentEntity> getAllPaymentMethods() {
+		try {
+			return entityManager.createNamedQuery("allPaymentMethods", PaymentEntity.class).getResultList();
+		} catch (NoResultException nre) {
+			return null;
+		}
+	}
+
+
+	public PaymentEntity getPaymentByUUID(String uuid) {
+		try {
+			return entityManager.createNamedQuery("paymentByUUID", PaymentEntity.class).setParameter("uuid", uuid).getSingleResult();
+		} catch (NoResultException nre) {
+			return null;
+		}
+	}
+}
